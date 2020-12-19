10bfc0f5485b3dd8d2c382788448eda8ba666a6e
medhabalani <iit2019021@iiita.ac.in>
2020-12-19 17:32:47 +0530

    seperate pages(ing,name) , well connected webpages.

--- files/forConnection.js
+++ files/forConnection.js
@@ -1,3 +1,60 @@
+var TxtRotate = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 300 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+  document.body.appendChild(css);
+};
+
 var userinput = document.getElementById('srch');
 
 document.getElementById('searching').addEventListener("click", function() {
@@ -30,7 +87,7 @@ function useData(data) {
                 <div class="btn-group">
                 <a href="${data.hits[0].recipe.shareAs}" class="btn btn-primary">Go somewhere</a>
                 </div>
-                
+
               </div>
             </div>
           </div>
@@ -45,7 +102,7 @@ function useData(data) {
               <div class="btn-group">
               <a href="${data.hits[1].recipe.shareAs}" class="btn btn-primary">Go somewhere</a>
               </div>
-              
+
             </div>
           </div>
         </div>
@@ -60,7 +117,7 @@ function useData(data) {
                 <div class="btn-group">
                 <a href="${data.hits[2].recipe.shareAs}" class="btn btn-primary">Go somewhere</a>
                 </div>
-                
+
               </div>
             </div>
           </div>
@@ -77,7 +134,7 @@ function useData(data) {
                 <div class="btn-group">
                 <a href="${data.hits[3].recipe.shareAs}" class="btn btn-primary">Go somewhere</a>
                 </div>
-                
+
               </div>
             </div>
           </div>
@@ -92,7 +149,7 @@ function useData(data) {
               <div class="btn-group">
               <a href="${data.hits[4].recipe.shareAs}" class="btn btn-primary">Go somewhere</a>
               </div>
-              
+
             </div>
           </div>
         </div>
@@ -107,12 +164,12 @@ function useData(data) {
                 <div class="btn-group">
                 <a href="${data.hits[5].recipe.shareAs}" class="btn btn-primary">Go somewhere</a>
                 </div>
-                
+
               </div>
             </div>
           </div>
         </div>
-        
+
       </div>
       <div class="row">
         <div class="col-md-4">
@@ -125,7 +182,7 @@ function useData(data) {
                 <div class="btn-group">
                 <a href="${data.hits[6].recipe.shareAs}" class="btn btn-primary">Go somewhere</a>
                 </div>
-                
+
               </div>
             </div>
           </div>
@@ -140,7 +197,7 @@ function useData(data) {
               <div class="btn-group">
               <a href="${data.hits[7].recipe.shareAs}" class="btn btn-primary">Go somewhere</a>
               </div>
-              
+
             </div>
           </div>
         </div>
@@ -155,12 +212,12 @@ function useData(data) {
                 <div class="btn-group">
                 <a href="${data.hits[8].recipe.shareAs}" class="btn btn-primary">Go somewhere</a>
                 </div>
-                
+
               </div>
             </div>
           </div>
         </div>
-        
+
       </div>
     </div>
     `

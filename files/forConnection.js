30e5c191c3f1778b37d09da26b2331a14f2bd257
xxx32 <iit2019032@iiita.ac.in>
2020-12-19 16:21:10 +0530

    input working

--- files/forConnection.js
+++ files/forConnection.js
@@ -1,3 +1,5 @@
+var userinput = document.getElementById('srch');
+
 document.getElementById('searching').addEventListener("click", function() {
     sendAPIRequest();
 });
@@ -5,7 +7,8 @@ document.getElementById('searching').addEventListener("click", function() {
 async function sendAPIRequest() {
     var APP_ID = "196ee461";
     var API_KEY = "28a90ed3d4a21ef85469ade6d3894ed1";
-    let response = await fetch(`https://api.edamam.com/search?app_id=${APP_ID}&app_key=${API_KEY}&q=pizza`);
+    let fetchTerm = `https://api.edamam.com/search?app_id=${APP_ID}&app_key=${API_KEY}&q=`+ userinput.value;
+    let response = await fetch(fetchTerm);
     console.log(response);
     let data = await response.json();
     console.log(data);
@@ -132,7 +135,7 @@ function useData(data) {
           <img class="card-img-top" src="${data.hits[7].recipe.image}" alt="Card image cap">
           <div class="card-body">
           <h5 class="card-title">${data.hits[7].recipe.label}</h5>
-          <p class="card-text">"${data.hits[7].recipe.ingredients[1].text}"</p>
+          <p class="card-text">"${data.hits[7].recipe.ingredients[7].text}"</p>
           <div class="d-flex justify-content-between align-items-center">
               <div class="btn-group">
               <a href="${data.hits[7].recipe.shareAs}" class="btn btn-primary">Go somewhere</a>
@@ -144,13 +147,13 @@ function useData(data) {
       </div>
       <div class="col-md-4">
           <div class="card mb-4 box-shadow">
-            <img class="card-img-top" src="${data.hits[2].recipe.image}" alt="Card image cap">
+            <img class="card-img-top" src="${data.hits[8].recipe.image}" alt="Card image cap">
             <div class="card-body">
-            <h5 class="card-title">${data.hits[2].recipe.label}</h5>
-            <p class="card-text">"${data.hits[2].recipe.ingredients[2].text}"</p>
+            <h5 class="card-title">${data.hits[8].recipe.label}</h5>
+            <p class="card-text">"${data.hits[8].recipe.ingredients[8].text}"</p>
             <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
-                <a href="${data.hits[2].recipe.shareAs}" class="btn btn-primary">Go somewhere</a>
+                <a href="${data.hits[8].recipe.shareAs}" class="btn btn-primary">Go somewhere</a>
                 </div>
                 
               </div>
